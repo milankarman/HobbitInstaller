a7275177db47bede284053901e53ec3ab58feb5e
Milan Karman <milankarman@gmail.com>
2021-12-23 11:52:17 +0100

    Added gitignore and removed generated files

--- HobbitInstaller/MainWindow.xaml.cs
+++ HobbitInstaller/MainWindow.xaml.cs
@@ -12,17 +12,35 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net.Http;
+using System.IO;
 
 namespace HobbitInstaller
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void btnInstall_Click(object sender, RoutedEventArgs e)
+        {
+            DownloadHobbitGame();
+            MessageBox.Show("Done");
+        }
+
+        private async void DownloadHobbitGame()
+        {
+            var httpClient = new HttpClient();
+
+            using (var stream = await httpClient.GetStreamAsync("https://via.placeholder.com/300.png"))
+            {
+                using (var fileStream = new FileStream("300.png", FileMode.CreateNew))
+                {
+                    await stream.CopyToAsync(fileStream);
+                }
+            }
         }
     }
 }
